3d2961fd375c1141ff83d2401f2ce0dcf4d83283
Collin Maillet <cmaillet@lcs.on.ca>
2025-03-31 14:31:38 -0400

    Initial Commit

---
+++ MVVMAndFreeformInput/MVVMAndFreeformInputApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMAndFreeformInputApp.swift
+//  MVVMAndFreeformInput
+//
+//  Created by Collin Maillet on 2025-03-31.
+//
+
+import SwiftUI
+
+@main
+struct MVVMAndFreeformInputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
